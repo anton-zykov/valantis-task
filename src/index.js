279899c82a3c8d35061623071a2836d9bcbbfc80
Zykov Anton <anton.zykov1@gmail.com>
2024-02-29 23:39:55 +0100

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './styles/main.css';
-import Page from './components/Page/Page';
-import Search from './components/Search/Search';
-
-const page = new Page();
-const search = new Search();
-document.body.append(search.element, page.element);
